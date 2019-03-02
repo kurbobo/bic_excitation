17ed7573142cbdcb3d6e9c3ec14b1d14f7575d70
kurbobo <kurbobo@yandex.ru>
2019-03-02 21:28:25 +0300

    dark mode excites without approximations in nonlinear regime but with chi2=0

--- evg_debug.py
+++ evg_debug.py
@@ -19,13 +19,14 @@ def zfunc_nonlean_B(z, t, delta, chi_2, chi_3, gamma_U, gamma_V, gamma_C, s, g):
 z0 = np.array([0.000001/np.sqrt(2), 0])
 delta=9e-02
 chi_2=0#3e-01
-chi_3=0#1
+chi_3=1#
 gamma_U=3e-01
 gamma_V=gamma_U
 gamma_C=3e-1
-s=0#3e-02
-start=400#39500
-dist=500
+s=3e-01
+#39500
+dist=10000
+start=dist-100
 stop=dist
 phi=0
 g=0.5
@@ -43,18 +44,20 @@ t_1,U_1r,U_2r=apx.circuit(delta=delta,
                                                           coef=coef,
                                                           phi=phi,
                                                    g=g)
-z, infodict = apx.odeintz(zfunc_nonlean_A, z0, t, args=(delta, chi_2, chi_3, gamma_U, gamma_V, gamma_C, s, g), full_output=True)
-C_1=z[:,0]
-C_2=z[:,1]
-A_1=C_1*np.exp(1j*(1+delta)*t)*xi
-A_2=C_2*np.exp(1j*(1+delta)*t)*xi
-# B_1=z[:,0]*np.exp(1j*delta*t)
-# B_2=z[:,1]*np.exp(1j*delta*t)
+z, infodict = apx.odeintz(zfunc_nonlean_B, z0, t, args=(delta, chi_2, chi_3, gamma_U, gamma_V, gamma_C, s, g), full_output=True)
+# C_1=z[:,0]
+# C_2=z[:,1]
+# A_1=C_1*np.exp(1j*(1+delta)*t)*xi
+# A_2=C_2*np.exp(1j*(1+delta)*t)*xi
+B_1=z[:,0]*np.exp(1j*(1+delta)*t)*xi
+B_2=z[:,1]*np.exp(1j*(1+delta)*t)*xi
 plt.clf()
 plt.plot(t_1[start:stop], (U_2r[start:stop]), label='A_2 real')
-plt.plot(t[start:stop],2*np.real(A_2)[start:stop], label='A_2_first_real_part')
-# plt.plot(t_1[start:stop], (U_1r[start:stop]), label='A_1 real')
-# plt.plot(t[start:stop],2*np.real(A_1)[start:stop], label='A_1_first_real_part')
+# plt.plot(t[start:stop],2*np.real(A_2)[start:stop], label='A_2_first')
+plt.plot(t[start:stop],2*np.real(B_1+B_2)[start:stop], label='B_1_plus_B_2_first')
+plt.plot(t_1[start:stop], (U_1r[start:stop]), label='A_1 real')
+# plt.plot(t[start:stop],2*np.real(A_1)[start:stop], label='A_1_first')
+plt.plot(t[start:stop],2*np.real(B_1-B_2)[start:stop], label='B_1_minus_B_2_first')
 plt.xlabel('t')
 plt.grid(True)
 plt.legend(loc='best')
