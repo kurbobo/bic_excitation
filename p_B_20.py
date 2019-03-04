7d79b008050c5a00f726feca46cd70a02ca9f7ee
kurbobo <kurbobo@yandex.ru>
2019-03-04 14:33:33 +0300

    formula, that gives us an amplitude of pumping depending on B_2 works correctly

--- p_B_20.py
+++ p_B_20.py
@@ -1,25 +1,18 @@
 from matplotlib import pyplot as plt
 import numpy as np
-Gamma= 5e-06
-alpha= 4.5001666666666666e-05
-sigma= 1.2499999875000003e-05
-sigma2= 1.2499999875000002e-09
-delta_0= 0.001
-B=np.arange(0.01,15,0.001)
-p=B*(Gamma+2*sigma)+1j*(2*sigma2+delta_0-2*alpha*B**2)*B
-plt.plot(np.abs(p),B)
-xplus=np.sqrt((2*(2*sigma2+delta_0)+np.sqrt((2*sigma2+delta_0)**2-3*(Gamma+2*sigma)**2))/(6*alpha))
-xminus=np.sqrt((2*(2*sigma2+delta_0)-np.sqrt((2*sigma2+delta_0)**2-3*(Gamma+2*sigma)**2))/(6*alpha))
-B_minus=np.sqrt((4*delta_0-2*np.sqrt(4*delta_0**2-3*Gamma**2))/alpha)
-B_plus=np.sqrt((4*delta_0+2*np.sqrt(4*delta_0**2-3*Gamma**2))/alpha)
-plt.axhline(y=xplus,color='red')
-plt.axhline(y=xminus,color='red')
-plt.axhline(y=B_plus,color='green')
-plt.axhline(y=B_minus,color='green')
-plt.xlabel('|p|')
-plt.ylabel('B_{20}')
-# plt.text(xminus+0.1,5,'$x_{-}$',fontsize=14,color='red')
-# plt.text(xplus+0.1,5,'$x_{+}$',fontsize=14,color='red')
-# plt.text(B_minus+0.1,5,'$y_{-}$',fontsize=14,color='green')
-# plt.text(B_plus+0.1,5,'$y_{+}$',fontsize=14,color='green')
-plt.show()
+xi=1
+Gamma=0.6# 6e-04
+alpha= 0.8333338333333
+delta_0=0.5
+gamma_C=1
+s=6
+# B=np.arange(0.5,1,0.000001)
+B=0.28430769429548375
+g=2*xi*B*np.sqrt((gamma_C * delta_0 - gamma_C*alpha*np.abs(B)**2 + Gamma + s)**2 + (delta_0 - alpha*np.abs(B)**2 - Gamma * gamma_C )**2)
+print(g)
+# plt.plot(np.abs(g),B)
+# plt.text(2,0.6,r"$\alpha=$"+str(alpha)+r"$ \gamma_C=$"+str(gamma_C)+r" s="+str(s))
+#
+# plt.xlabel('|g|')
+# plt.ylabel('B_{20}')
+# plt.show()
